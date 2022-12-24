aa3ab24b8e8ad1d2affeb966a2a9a90062817ffe
Artem Nazarov <art.nazar.11@gmail.com>
2022-12-24 14:10:23 +0300

    fixed the tasks according to the review [2]

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -6,7 +6,6 @@ import common.Vacancy;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
@@ -15,11 +14,9 @@ public class Task7 {
 
   public static Set<String> vacancyNames(Collection<Company> companies) {
     return companies.stream()
-            .flatMap(Task7::getVacancyTitlesForOneCompany)
+            .map(Company::getVacancies)
+            .flatMap(Collection::stream)
+            .map(Vacancy::getTitle)
             .collect(Collectors.toSet());
-  }
-
-  public static Stream<String> getVacancyTitlesForOneCompany(Company company) {
-    return company.getVacancies().stream().map(Vacancy::getTitle);
   }
 }
--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -21,6 +21,7 @@ public class Task8 {
 
   //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
   public List<String> getNames(List<Person> persons) {
+    if (persons == null) return Collections.emptyList();
     return persons.stream().skip(1)
             .map(Person::getFirstName)
             .collect(Collectors.toList());
@@ -33,6 +34,7 @@ public class Task8 {
 
   //Для фронтов выдадим полное имя, а то сами не могут
   public String convertPersonToString(Person person) {
+    if (person == null) return "";
     return Stream.of(person.getSecondName(), person.getFirstName(), person.getMiddleName())
             .filter(Objects::nonNull)
             .filter(fullNamePart -> !fullNamePart.isBlank())
@@ -41,22 +43,22 @@ public class Task8 {
 
   // словарь id персоны -> ее имя
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    persons.stream()
+    if (persons == null) return Collections.emptyMap();
+    return persons.stream()
             .filter(Objects::nonNull)
-            .forEach(person -> map.putIfAbsent(person.getId(), convertPersonToString(person)));
-    return map;
+            .collect(Collectors.toMap(Person::getId, this::convertPersonToString, (a, b) -> a));
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    Set<Person> unitedSet = new HashSet<>(persons1);
-    unitedSet.addAll(persons2);
-    return unitedSet.size() < persons1.size() + persons2.size();
+    if (persons1 == null || persons2 == null) return false;
+    //return Collections.disjoint(persons1, persons2);
+    return persons1.stream().anyMatch(new HashSet<>(persons2)::contains);
   }
 
   //...
   public long countEven(Stream<Integer> numbers) {
+    if (numbers == null) return 0;
     return numbers.filter(Objects::nonNull)
             .filter(number -> number % 2 == 0)
             .count();
